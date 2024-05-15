93a72bfb90ceccf6d2b2a9afc7dc97169f5f2c9a
GugaGordeladze <gugagordeladze@yahoo.com>
2024-05-15 15:43:30 +0400

    Add files via upload

---
+++ send.js
@@ -0,0 +1,48 @@
+function sendSMS() {
+  console.log("Sending SMS...");
+  const personNumber = document.getElementById('numberInput').value;
+  const textInput = document.getElementById('textInput').value;
+
+  fetch('https://api.twilio.com/2010-04-01/Accounts/ACd2799421f889c5a61dc6dc8ed5c31dec/Messages.json', {
+      method: 'POST',
+      headers: {
+          'Authorization': 'Basic ' + btoa('ACd2799421f889c5a61dc6dc8ed5c31dec:2d662aaf6b325c9fb9dfce76f356de15'),
+          'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+          From: '+16504259178',
+          To: personNumber,
+          Body: textInput
+      })
+  })
+  .then(response => {
+      console.log("Response:", response);
+      if (response.ok) {
+          console.log("SMS sent successfully");
+          displayAlert("SMS sent successfully", "success");
+          // Handle success, e.g., show a success message to the user
+      } else {
+          console.error("Failed to send SMS");
+          displayAlert("Failed to send SMS", "error");
+          // Handle failure, e.g., show an error message to the user
+      }
+  })
+  .catch(error => {
+      console.error("Error:", error);
+      displayAlert("Error occurred while sending SMS", "error");
+      // Handle error, e.g., show an error message to the user
+  });
+}
+
+function displayAlert(message, type) {
+  const alertMessage = document.getElementById('alertMessage');
+  alertMessage.textContent = message;
+  alertMessage.classList.add(type);
+  alertMessage.style.display = "block";
+
+  // Hide the alert after 3 seconds
+  setTimeout(() => {
+      alertMessage.style.display = "none";
+      alertMessage.classList.remove(type);
+  }, 3000);
+}
