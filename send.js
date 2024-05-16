900cb1cad300c2ec4344f44f3efcb2a0eacbc21d
GugaGordeladze <gugagordeladze@yahoo.com>
2024-05-16 11:02:54 +0400

    Update send.js

--- send.js
+++ send.js
@@ -3,10 +3,10 @@ function sendSMS() {
   const personNumber = document.getElementById('numberInput').value;
   const textInput = document.getElementById('textInput').value;
 
-  fetch('https://api.twilio.com/2010-04-01/Accounts/SK0542431a25749409aa64461c2b438260/Messages.json', {
+  fetch('https://api.twilio.com/2010-04-01/Accounts/ACd2799421f889c5a61dc6dc8ed5c31dec/Messages.json', {
       method: 'POST',
       headers: {
-          'Authorization': 'Basic ' + btoa('SK0542431a25749409aa64461c2b438260:0iSEn9msabq7sV44UTJxMJUmGwJ1CXLx'),
+          'Authorization': 'Basic ' + btoa('SK0542431a25749409aa64461c2b438260:2225af7e5b6ab7a039111f419b806814'),
           'Content-Type': 'application/x-www-form-urlencoded'
       },
       body: new URLSearchParams({
